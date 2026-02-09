bd6730320e943c60362cacbab17a5070b108d1a8
Arpit Mishra <5690607+thenextfreelancer@users.noreply.github.com>
2026-02-09 15:19:58 +0530

    Implement error handling for Snowflake connection
    Added error handling for database connection and data retrieval.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,27 +12,30 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+try:
+    cnx = st.connection("snowflake")
+    session = cnx.session()
+    my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+    #st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect(
-    'Choose up to 5 ingredients:',
-    my_dataframe,
-    max_selections=5
-)
+    ingredients_list = st.multiselect(
+        'Choose up to 5 ingredients:',
+        my_dataframe,
+        max_selections=5
+    )
 
-if(ingredients_list):
-    ingredients_string = ''
+    if(ingredients_list):
+        ingredients_string = ''
 
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen
+        for fruit_chosen in ingredients_list:
+            ingredients_string += fruit_chosen
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('"""+ ingredients_string+"""', '"""+ name_on_order+"""') """
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('"""+ ingredients_string+"""', '"""+ name_on_order+"""') """
 
-    #st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
+        #st.write(my_insert_stmt)
+        time_to_insert = st.button('Submit Order')
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered!', icon="✅")
+except Exception as error:
+    print("An error occurred:", error)
