0665d654fbd5f8538b949fe5f730184459854890
Arpit Mishra <5690607+thenextfreelancer@users.noreply.github.com>
2026-02-09 16:03:06 +0530

    Fix dataframe function call for Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,4 @@ except Exception as error:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = sr.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
